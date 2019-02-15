e9c55c69fa56ee7c7bcfb07937227a76ecf090df
Raghu Betina <raghu@firstdraft.com>
2019-02-15 15:53:12 +0000

    Generate brands resource with draft:resource

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,63 @@
+class BrandsController < ApplicationController
+  def index
+    @brands = Brand.all
+
+    render("brand_templates/index.html.erb")
+  end
+
+  def show
+    @brand = Brand.find(params.fetch("id_to_display"))
+
+    render("brand_templates/show.html.erb")
+  end
+
+  def new_form
+    @brand = Brand.new
+
+    render("brand_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @brand = Brand.new
+
+    @brand.name = params.fetch("name")
+    @brand.description = params.fetch("description")
+
+    if @brand.valid?
+      @brand.save
+
+      redirect_back(:fallback_location => "/brands", :notice => "Brand created successfully.")
+    else
+      render("brand_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @brand = Brand.find(params.fetch("prefill_with_id"))
+
+    render("brand_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @brand = Brand.find(params.fetch("id_to_modify"))
+
+    @brand.name = params.fetch("name")
+    @brand.description = params.fetch("description")
+
+    if @brand.valid?
+      @brand.save
+
+      redirect_to("/brands/#{@brand.id}", :notice => "Brand updated successfully.")
+    else
+      render("brand_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @brand = Brand.find(params.fetch("id_to_remove"))
+
+    @brand.destroy
+
+    redirect_to("/brands", :notice => "Brand deleted successfully.")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Brand resource:
+
+  # CREATE
+  get("/brands/new", { :controller => "brands", :action => "new_form" })
+  post("/create_brand", { :controller => "brands", :action => "create_row" })
+
+  # READ
+  get("/brands", { :controller => "brands", :action => "index" })
+  get("/brands/:id_to_display", { :controller => "brands", :action => "show" })
+
+  # UPDATE
+  get("/brands/:prefill_with_id/edit", { :controller => "brands", :action => "edit_form" })
+  post("/update_brand/:id_to_modify", { :controller => "brands", :action => "update_row" })
+
+  # DELETE
+  get("/delete_brand/:id_to_remove", { :controller => "brands", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Product resource:
 
   # CREATE
