40767fa8d6ac091c0b44f8ba51babc24b4b4ba4d
Raghu Betina <raghu@firstdraft.com>
2019-02-15 15:54:44 +0000

    Whitelist columns in Solution for ActiveAdmin

--- app/admin/solutions.rb
+++ app/admin/solutions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Solution do
+
+ permit_params :complain_id, :message, :close, :user_id, :like, :scomment_id, :title
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
