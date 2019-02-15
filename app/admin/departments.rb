1472c275a77bfca3cd583bef2b203ce9d822e2e2
Raghu Betina <raghu@firstdraft.com>
2019-02-15 15:54:35 +0000

    Whitelist columns in Department for ActiveAdmin

--- app/admin/departments.rb
+++ app/admin/departments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Department do
+
+ permit_params :code, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
