95b452eccb9d6f0ad6b8c002f855af27ac1394e7
Raghu Betina <raghu@firstdraft.com>
2019-02-15 15:54:16 +0000

    Whitelist columns in Region for ActiveAdmin

--- app/admin/regions.rb
+++ app/admin/regions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Region do
+
+ permit_params :state, :state_code, :statenmbr, :salsterr_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
