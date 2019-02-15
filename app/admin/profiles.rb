ab58ccaf3bec8856d8725d8a754ac3a82dbd0e6d
Raghu Betina <raghu@firstdraft.com>
2019-02-15 15:54:04 +0000

    Whitelist columns in Profile for ActiveAdmin

--- app/admin/profiles.rb
+++ app/admin/profiles.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Profile do
+
+ permit_params :user_id, :name, :last_name, :nick_name, :phone, :mobile, :profile_pic, :rank_id, :city_base, :address, :active, :slprsnid, :salsterr_id, :start, :full_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
